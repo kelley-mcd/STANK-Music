d07332d3746b187f15710ed526d33c3bec45c54a
Alex Parker <alex.n.parker82@gmail.com>
2021-07-31 15:21:18 -0400

    genre input functionality

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,56 @@
-const kanyeBtn$ = $('.kanye-btn');
-const userBtn$ = $('.user-btn');
-const userInput$ = $('select');
+$(document).ready(function () {
+  const kanyeBtn$ = $('.kanye-btn');
+  const userBtn$ = $('.user-btn');
+  const userInput$ = $('select');
+
+  function handleGenerateOptions() {
+    let url = 'https://api.napster.com/v2.2/genres?apikey=OTI0NjE5NWEtNWVjOC00ZTJjLTliMDgtOTdkMTg5NjEwYmU0';
+    let genreArray = [];
+
+    fetch(url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data, genres) {
+        console.log(data.genres);
+        for (var i = 0; i < data.genres.length; i++) {
+          let options = data.genres[i].name
+          console.log(options);
+          let userOptions = document.createElement('option');
+          userOptions.textContent = options;
+          userOptions.value = options;
+          userInput$.append(userOptions);
+        }
+      })
+  };
 
 
-function handleUserFormSubmit(event) {
-  event.preventDefault();
 
-  var searchInputVal = userInput$.value;
-  
 
-  if (!searchInputVal) {
-    alert('You need a search input value!');
-    return;
-  }
+  function handleUserFormSubmit(event) {
+    event.preventDefault();
 
-  let queryString = `./user-form.html?q=${searchInputVal}`;
+    var searchInputVal = $('option').value;
 
-  location.assign(queryString);
-};
 
-function handleKanyeButton(event) {
-  let queryString = "./kanye-decide.html";
-  location.assign(queryString);
-};
+    if (!searchInputVal) {
+      alert('You need a search input value!');
+      return;
+    }
 
-userBtn$.click(handleUserFormSubmit);
-kanyeBtn$.click(handleKanyeButton);
+    let queryString = `./user-form.html?q=${searchInputVal}`;
 
-$(document).ready(function(){
-  $('select').formSelect();
+    location.assign(queryString);
+  };
+
+  function handleKanyeButton(event) {
+    let queryString = "./kanye-decide.html";
+    location.assign(queryString);
+  };
+
+  userBtn$.click(handleUserFormSubmit);
+  kanyeBtn$.click(handleKanyeButton);
+  handleGenerateOptions();
+
+  userInput$.formSelect();
 });
